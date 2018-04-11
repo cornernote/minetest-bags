b77a449aeb9d346d2afeaaa8dca35ea865c4cb06
TeTpaAka <6652859+TeTpaAka@users.noreply.github.com>
2018-04-11 12:59:39 +0200

    Fix detached inventories being send to all players

--- bags/init.lua
+++ bags/init.lua
@@ -120,7 +120,7 @@ minetest.register_on_joinplayer(function(player)
 		allow_move = function(inv, from_list, from_index, to_list, to_index, count, player)
 			return 0
 		end,
-	})
+	}, player:get_player_name())
 	for i=1,4 do
 		local bag = "bag"..i
 		player_inv:set_size(bag, 1)
